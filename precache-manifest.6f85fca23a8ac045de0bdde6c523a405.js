c088785c8abb4fed25fdb560a51de984e2dd59d6
attiliogiglio <attilio.g93@gmail.com>
2020-09-01 13:30:55 -0400

    Updates

---
+++ precache-manifest.6f85fca23a8ac045de0bdde6c523a405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db44119b5d67907e99c30080c36237e",
+    "url": "/todolist-react-fetch/index.html"
+  },
+  {
+    "revision": "b65efc26c11d797147d9",
+    "url": "/todolist-react-fetch/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "14a5c46146d859914fb5",
+    "url": "/todolist-react-fetch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b65efc26c11d797147d9",
+    "url": "/todolist-react-fetch/static/js/2.bfc3751e.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/todolist-react-fetch/static/js/2.bfc3751e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a5c46146d859914fb5",
+    "url": "/todolist-react-fetch/static/js/main.4691a6db.chunk.js"
+  },
+  {
+    "revision": "233398da9aca14a2e157",
+    "url": "/todolist-react-fetch/static/js/runtime-main.8e841c73.js"
+  }
+]);
